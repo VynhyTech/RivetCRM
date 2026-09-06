06c99c4055cb41999e846dfca0b32b9b3fd4bdb7
VynhyTech <vynhytechnologies@gmail.com>
2026-09-06 18:46:04 +0400

    Add Rivet branding assets

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = Number(process.env.PORT) || 10000;
 const contentTypes = {
   '.css': 'text/css; charset=utf-8',
   '.html': 'text/html; charset=utf-8',
+  '.jpg': 'image/jpeg',
   '.js': 'text/javascript; charset=utf-8',
   '.json': 'application/json; charset=utf-8',
   '.png': 'image/png',
